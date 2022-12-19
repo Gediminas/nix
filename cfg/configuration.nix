43e0f1831804816e4c59f190ffd285ba5b931bff
Gediminas Lūžys <gediminas.luzys@gmail.com>
2022-12-19 17:41:30 +0200

    Add module covering hardware quirks

--- cfg/configuration.nix
+++ cfg/configuration.nix
@@ -1,12 +1,6 @@
 { config, pkgs, lib, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      #<home-manager/nixos>
-    ];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
--- flake.nix
+++ flake.nix
@@ -2,53 +2,64 @@
   description = "NixOS System Config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # NixOS
+    nixpkgs.url          = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Home-Manager
+    #url = "github:nix-community/home-manager";
     home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     #flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, nixos-hardware, ... }:
     let
-
-      user_gds_cfg_path = ./cfg/user_gds.nix;
-      system_cfg_path = ./cfg/configuration.nix;
-
       system = "x86_64-linux";
-
-      pkgs = import nixpkgs {
+      lib    = nixpkgs.lib;
+      pkgs   = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
-
-      lib = nixpkgs.lib;
-
     in {
-      homeManagerConfigurations = {
-        gds = home-manager.lib.homeManagerConfiguration {
-          #pkgs = nixpkgs.legacyPackages.${system};
-          pkgs = pkgs;
-          modules = [
-            user_gds_cfg_path 
-            #./some-extra-module.nix
-            {
-              home = {
-                username = "gds";
-                homeDirectory = "/home/gds";
-                stateVersion = "22.11";
-              };
-            }
-          ];
-        };
+      homeManagerConfigurations.gds = home-manager.lib.homeManagerConfiguration {
+        #pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = pkgs;
+        modules = [
+
+          # Home-manager user config
+          ./cfg/user_gds.nix 
+
+          {
+            home = {
+              username = "gds";
+              homeDirectory = "/home/gds";
+              stateVersion = "22.11";
+            };
+          }
+        ];
       };
-      nixosConfigurations = {
-        nixos = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ system_cfg_path ];
-        };
+
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+
+          # System config
+          ./cfg/configuration.nix
+
+          # Scanned Hardware
+          ./cfg/hardware-configuration.nix
+
+          # Hardware quirks
+          # https://github.com/NixOS/nixos-hardware/blob/master/flake.nix
+          # nixos-hardware.nixosModules.lenovo-thinkpad-t490
+          # nixos-hardware.nixosModules.lenovo-thinkpad-x1
+          # nixos-hardware.nixosModules.lenovo-thinkpad-x1-10th-gen
+          nixos-hardware.nixosModules.apple-macbook-pro-14-1
+        ];
       };
     };
 }
