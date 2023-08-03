e4b74a8dba739305dce53bb133644106a797eb52
Gediminas Lūžys <gediminas.luzys@gmail.com>
2023-08-04 00:07:59 +0300

    NixOS update

--- ThinkPad_X1/configuration.nix
+++ ThinkPad_X1/configuration.nix
@@ -31,7 +31,7 @@
 # TODO
 # echo  'ACTION=="add",SUBSYSTEM=="input",ATTR{name}=="TPPS/2 IBM TrackPoint",ATTR{device/drift_time}="25"'  > /etc/udev/rules.d/10-trackpoint.rules
 
-{ config, pkgs, lib, ... }: {
+{ pkgs, ... }: {
   system.stateVersion = "23.05";
 
   boot.loader.systemd-boot.enable = true;
